d87d2e7c94d87706d0473dc1749ba367d84a6d3d
Dewan Mehedi Hasan <tanjildewan10@gmail.com>
2026-01-12 21:41:42 +0600

    add domain in settings

--- online_course_management/settings.py
+++ online_course_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nc358+)o^=874l^9cz-=eza+m^qcl8j41+oh09t2&tx-tk=+n1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["skillyfy-backend.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
